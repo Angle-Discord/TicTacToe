a5660bfce2c556115eafa6c5aff249dcc5e88003
Angel Garcia <rl230261@stu.rlas-116.org>
2019-09-16 12:15:41 -0500

    init commit2

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by ANGEL GARCIA on 9/9/19.
+//  Copyright © 2019 ANGEL GARCIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
